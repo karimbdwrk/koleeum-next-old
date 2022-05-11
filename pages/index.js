1e8a6091a19f97cc759cefc18d98bca5bfad4cb5
Karim Badwork <karim@badwork.fr>
2022-05-12 00:45:30 +0200

    disable eslint rules

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://koleeum.immo" target="_blank">Koleeum.immo V2 in NextJS!</a>
+          Welcome to <a href="https://koleeum.immo" target="_blank" rel="noreferrer">Koleeum.immo V2 in NextJS!</a>
         </h1>
 
         <p className={styles.description}>
