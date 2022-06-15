fff595991c4c71e0e14d5a6bbef70486246306bb
Karim Badwork <karim@badwork.fr>
2022-06-15 22:28:57 +0200

    next config swc

--- dist/next.config.dev.js
+++ dist/next.config.dev.js
@@ -2,11 +2,8 @@
 
 module.exports = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  },
+  swcMinify: false,
+  // it should be false by default 
   images: {
     domains: ['koleeum-aws-bucket.s3.eu-west-3.amazonaws.com']
   }
