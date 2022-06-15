9171785afb55a6341243ffd602a06f67d80b8281
Karim Badwork <karim@badwork.fr>
2022-06-15 22:41:23 +0200

    reurn to old version

--- dist/next.config.dev.js
+++ dist/next.config.dev.js
@@ -2,8 +2,6 @@
 
 module.exports = {
   reactStrictMode: true,
-  swcMinify: false,
-  // it should be false by default 
   images: {
     domains: ['koleeum-aws-bucket.s3.eu-west-3.amazonaws.com']
   }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: false, // it should be false by default 
   images: {
     domains: ['koleeum-aws-bucket.s3.eu-west-3.amazonaws.com'],
   }
